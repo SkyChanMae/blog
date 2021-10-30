e72c34ccc5616a650d84f777e5b0b9747e81963a
SkyChanMae <1191803480@qq.com>
2021-10-30 21:45:57 +0800

    '很多东西的配置'

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,19 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 
 import logging
 
-logger=logging.getLogger('django')
-from django.http import HttpResponse
-def log(request):
+#logger=logging.getLogger('django')
+#from django.http import HttpResponse
+#def log(request):
 
-    logger.info('info')
-    return HttpResponse('test')
+#    logger.info('info')
+#    return HttpResponse('test')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',log)
+    path('',include(('users.urls','users'),namespace='users'))
+    #path('',log)
 ]
