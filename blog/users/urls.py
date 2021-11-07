6fecd84c5efe3d06dbf772edd5aea0516f01d125
SkyChanMae <1191803480@qq.com>
2021-11-07 14:38:11 +0800

    '短信验证码'

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView,ImageCodeView
 urlpatterns = [
-    path('register/',RegisterView.as_view(),name='register')
+    path('register/',RegisterView.as_view(),name='register'),
+
+
+
+    path('imagecode/',ImageCodeView.as_view(),name='imagecode'),
 ]
+
+
--- blog/users/views.py
+++ blog/users/views.py
@@ -9,3 +9,24 @@ class RegisterView(View):
     def get(self,request):
 
         return render(request,'register.html')
+
+
+from django.http.response import HttpResponseBadRequest
+from libs.captcha.captcha import captcha
+from django_redis import get_redis_connection
+from django.http import HttpResponse
+class ImageCodeView(View):
+
+    def get(self,request):
+
+        uuid = request.GET.get('uuid')
+
+        if uuid is None:
+            return HttpResponseBadRequest('没有传递uuid')
+
+        text,image = captcha.generate_captcha()
+
+        redis_conn = get_redis_connection('default')
+        redis_conn.setex('img:%s'%uuid,300,text)
+
+        return HttpResponse(image,content_type='image/jpeg')
