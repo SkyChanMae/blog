07ed9a5058d47d0763f529213500d44af1adda36
SkyChanMae <1191803480@qq.com>
2021-11-10 23:15:43 +0800

    '实现短信验证码发送'

--- blog/users/views.py
+++ blog/users/views.py
@@ -2,13 +2,51 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
-
+from django.http.response import HttpResponseBadRequest
+import re
+from users.models import User
+from django.db import DatabaseError
+import logging
+logger=logging.getLogger('django')
 
 class RegisterView(View):
 
     def get(self,request):
 
         return render(request,'register.html')
+
+#     def post(self,request):
+#
+#         mobile=request.POST.get('mobile')
+#         password=request.POST.get('password')
+#         password2=request.POST.get('password2')
+#         smscode=request.POST.get('sms_code')
+#
+#         if not all([mobile,password,password2,smscode]):
+#             return HttpResponseBadRequest('缺少必要的参数')
+#
+#         if not re.match(r'^1[3-9]\d{9}$',mobile):
+#             return HttpResponseBadRequest('手机号不符合规则')
+#
+#         if not re.match(r'^[0-9A-Za-z]{8,20}$',password):
+#             return HttpResponseBadRequest('请输入正确的8-20位密码，数字和字母')
+#
+#         if password != password2:
+#             return HttpResponseBadRequest('输入密码不一致')
+#
+#         redis_conn = get_redis_connection('default')
+#         redis_sms_code = redis_conn.get('sms:%s'%mobile)
+#         if redis_sms_code is None:
+#             return HttpResponseBadRequest('短信验证码已过期')
+#         if smscode != redis_sms_code.decode():
+#             return HttpResponseBadRequest('短信验证码不一致')
+#
+#         try:
+#             user=User.objects.create_user(username=mobile,mobile=mobile,password=password)
+#         except DatabaseError as e:
+#             return HttpResponseBadRequest('注册失败')
+#
+#         return HttpResponse('注册成功，重定向到首页')
 
 
 from django.http.response import HttpResponseBadRequest
@@ -31,3 +69,42 @@ class ImageCodeView(View):
         redis_conn.setex('img:%s'%uuid,300,text)
 
         return HttpResponse(image,content_type='image/jpeg')
+
+
+from django.http.response import JsonResponse
+from utils.response_code import RETCODE
+from random import randint
+from libs.yuntongxun.sms import CCP
+
+class SmsCodeView(View):
+
+    def get(self,request):
+
+        mobile=request.GET.get('mobile')
+        image_code=request.GET.get('image_code')
+        uuid=request.GET.get('uuid')
+
+        if not all([mobile,image_code,uuid]):
+            return JsonResponse({'code':RETCODE.NECESSARYPARAMERR,'errmsg':'缺少必要的参数'})
+
+        redis_conn=get_redis_connection('default')
+        redis_image_code=redis_conn.get('img:%s'%uuid)
+
+        if redis_image_code is None:
+            return JsonResponse({'code':RETCODE.IMAGECODEERR,'errmsg':'图片验证码已过期'})
+
+        try:
+            redis_conn.delete('img:%s'%uuid)
+        except Exception as e:
+            logger.error(e)
+
+        if redis_image_code.decode().lower() != image_code.lower():
+            return JsonResponse({'code':RETCODE.IMAGECODEERR,'errmsg':'图片验证码错误'})
+
+        sms_code='%06d'%randint(0,999999)
+        logger.info(sms_code)
+        redis_conn.setex('sms:%s'%mobile,300,sms_code)
+
+        CCP().send_template_sms(mobile,[sms_code,5],1)
+
+        return JsonResponse({'code':RETCODE.OK,'errmsg':'短信发送成功'})
