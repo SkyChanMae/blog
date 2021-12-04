b0cbd08e18942461cfab403b6c58b6819dc64c2f
SkyChanMae <1191803480@qq.com>
2021-12-04 13:55:03 +0800

    '文章分类模型'

--- blog/home/models.py
+++ blog/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class ArticleCategory(models.Model):
+
+    title = models.CharField(max_length=100, blank=True)
+
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'tb_category'
+        verbose_name = '类别管理'
+        verbose_name_plural = verbose_name
