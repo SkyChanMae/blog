f7f5d27252155578ea59b6ee31e15218428d3b4d
SkyChanMae <1191803480@qq.com>
2021-12-04 14:41:38 +0800

    '文章栏目渲染'

--- blog/users/views.py
+++ blog/users/views.py
@@ -275,11 +275,18 @@ class UserCenterView(LoginRequiredMixin,View):
 
 
 
-class WriteBlogView(View):
+from home.models import ArticleCategory
+class WriteBlogView(LoginRequiredMixin,View):
 
     def get(self,request):
 
-        return render(request,'write_blog.html')
+        categories = ArticleCategory.objects.all()
+
+        context = {
+            'categories': categories
+        }
+
+        return render(request,'write_blog.html',context=context)
 
 
 
