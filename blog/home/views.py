e0e3f40f39f73a32e6b3cf6e4716b14d7b773db9
SkyChanMae <1191803480@qq.com>
2021-12-06 23:56:06 +0800

    '首页分类数据高亮展示'

--- blog/home/views.py
+++ blog/home/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
+from home.models import ArticleCategory,Article
+from django.http.response import HttpResponseNotFound
 # Create your views here.
 class IndexView(View):
     def get(self,request):
 
-        return render(request,'index.html')
+        categories = ArticleCategory.objects.all()
+
+        cat_id = request.GET.get('cat_id', 1)
+
+        try:
+            category = ArticleCategory.objects.get(id=cat_id)
+        except ArticleCategory.DoesNotExist:
+            return HttpResponseNotFound('没有此分类')
+
+        page_num = request.GET.get('page_num', 1)
+        page_size = request.GET.get('page_size', 10)
+
+        articles = Article.objects.filter(category=category)
+
+        from django.core.paginator import Paginator, EmptyPage
+        paginator = Paginator(articles, per_page=page_size)
+
+        try:
+            page_articles = paginator.page(page_num)
+        except EmptyPage:
+            return HttpResponseNotFound('empty page')
+
+        total_page = paginator.num_pages
+
+        context = {
+            'categories': categories,
+            'category': category,
+            'articles': page_articles,
+            'page_size': page_size,
+            'total_page': total_page,
+            'page_num': page_num
+        }
+
+        return render(request,'index.html',context=context)
