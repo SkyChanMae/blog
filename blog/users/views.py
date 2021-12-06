1fd15ffefc5643fa4fc9bdc428675b0661cece2d
SkyChanMae <1191803480@qq.com>
2021-12-06 23:35:18 +0800

    '博客发布'

--- blog/users/views.py
+++ blog/users/views.py
@@ -275,7 +275,7 @@ class UserCenterView(LoginRequiredMixin,View):
 
 
 
-from home.models import ArticleCategory
+from home.models import ArticleCategory,Article
 class WriteBlogView(LoginRequiredMixin,View):
 
     def get(self,request):
@@ -287,6 +287,46 @@ class WriteBlogView(LoginRequiredMixin,View):
         }
 
         return render(request,'write_blog.html',context=context)
+
+    def post(self, request):
+
+        avatar = request.FILES.get('avatar')
+
+        title = request.POST.get('title')
+
+        category_id = request.POST.get('category')
+
+        tags = request.POST.get('tags')
+
+        sumary = request.POST.get('sumary')
+
+        content = request.POST.get('content')
+
+        user = request.user
+
+        if not all([avatar, title, category_id, sumary, content]):
+            return HttpResponseBadRequest('参数不全')
+
+        try:
+            category = ArticleCategory.objects.get(id=category_id)
+        except ArticleCategory.DoesNotExist:
+            return HttpResponseBadRequest('没有此分类')
+
+        try:
+            article = Article.objects.create(
+                author=user,
+                avatar=avatar,
+                title=title,
+                category=category,
+                tags=tags,
+                sumary=sumary,
+                content=content
+            )
+        except Exception as e:
+            logger.error(e)
+            return HttpResponseBadRequest('发布失败，请稍后再试')
+
+        return redirect(reverse('home:index'))
 
 
 
